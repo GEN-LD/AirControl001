e95e4df917743789ea17b536de57c14b8f14cff7
dgliang <1138421194@qq.com>
2018-05-14 00:02:02 +0800

    图表样式改善一点点～～

--- app/src/main/java/com/scut/toothpick/aircontrol001/Activities/HabitChartActivity.java
+++ app/src/main/java/com/scut/toothpick/aircontrol001/Activities/HabitChartActivity.java
@@ -1,10 +1,12 @@
 package com.scut.toothpick.aircontrol001.Activities;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -28,13 +30,21 @@ public class HabitChartActivity extends AppCompatActivity {
             entries.add(new Entry(mydata[i],i));
         }
         LineDataSet lineDataSet = new LineDataSet(entries,"测试图表");
-        lineDataSet.setColor(1111111);
+        lineDataSet.setColor(Color.parseColor("#111111"));
         String[] xval = {"一月","二月","三月","四月","五月","六月","七月","八月"};
         LineData lineData = new LineData(xval,lineDataSet);
         lineChart.setData(lineData);
-
+        //设置图表外观
+        lineChart.setDrawBorders(false);
+        lineChart.setScaleEnabled(false);   //是否可以缩放XY轴
         XAxis xAxis = lineChart.getXAxis();
         xAxis.setDrawGridLines(false);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);//设置X轴数据位置
+        YAxis yAxis0 = lineChart.getAxisLeft();
+        YAxis yAxis1 = lineChart.getAxisRight();
+        yAxis1.setDrawAxisLine(false);//绘制轴线
+        yAxis0.setDrawGridLines(false);//绘制网格线
+        yAxis1.setDrawGridLines(false);
         lineChart.invalidate();
     }
 
